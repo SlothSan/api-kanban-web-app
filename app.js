227426712a0e668101851bb4d490d2e0927b74df
Mike Oram <Mikeoram01@gmail.com>
2022-11-23 11:58:52 +0000

    Added mongodb package

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const port = 3000;
+const url = 'mongodb://root:password@localhost:27017';
 
 app.use(express.json());
 
