8edeaabbc6b17e6da832a15ba8e85c4ab7e47437
Mike Oram <Mikeoram01@gmail.com>
2022-11-23 11:35:47 +0000

    Installed express and tested

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    console.log('just-testing');
+})
+
+
+app.listen(port);
